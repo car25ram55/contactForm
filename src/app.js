df237cd4c83cdb7e858f0d441651dd04e422acb1
camo55 <car25ram@gmail.com>
2024-02-23 20:56:57 -0500

    commit1

--- src/app.js
+++ src/app.js
@@ -1,11 +1,37 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// import "bootstrap";
+// import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  emailjs.init({
+    publicKey: "ZPoRP6myJxqtvQ1_G"
+  });
+
+  const button = document.querySelector("#send-info");
+  button.addEventListener("click", sendEmail);
 };
+
+function sendEmail() {
+  const name = document.querySelector("#name-input").value;
+  const email = document.querySelector("#email-input").value;
+  const message = document.querySelector("#message-input").value;
+
+  // To Company from Customer
+  emailjs.send("service_u8xewyl", "template_0jtz59k", {
+    to_name: name,
+    from_email: email
+  });
+
+  emailjs.send("service_u8xewyl", "template_wk2j2a9", {
+    to_name: name,
+    from_email: email,
+    message: message
+  });
+}
+
+document.querySelector("#name-input").value = "";
+document.querySelector("#email-input").value = "";
+document.querySelector("#message-input").value = "";
